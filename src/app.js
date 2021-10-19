65d1d018fa0ed118eb4fb5960f9fa8f43853d069
ozlem goksu <ozlemgoksu4@gmail.com>
2021-10-19 18:21:45 +0300

    18 Ekim Ders sonu

---
+++ src/app.js
@@ -0,0 +1,40 @@
+console.log("Merhaba slm")
+
+let  dolarBugun = 9.50
+
+let dolarDun = 9.20
+
+{
+    let dolarDun = 9.10
+}
+
+
+console.log(dolarDun)
+
+
+const euroDun = 11.2
+//euroDun = 11
+
+console.log(euroDun)
+
+//array
+//camelCasing
+//PascalCasing
+
+let konutKredileri = ["konut kredisi","emlak kredisi","kamu kredisi","ozel kredi"]
+
+for (let i = 0; i <konutKredileri.length ; i++) {
+    console.log("<li>"+konutKredileri[i]+" </li>")
+    
+    
+}
+
+
+console.log(konutKredileri)
+
+
+
+
+
+
+
